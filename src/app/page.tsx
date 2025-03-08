6e95b98facfc3f62ebaccb963f8662a1e1adb068
sjcode99 <surajlal261199@gmail.com>
2025-03-08 16:59:15 +0530

    add product api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+"use client";
 import ContentHeadSection from "@/components/content-header-section";
 import { Separator } from "@/components/ui/separator";
 import CategorySection from "@/components/browse-category-section";
@@ -6,25 +6,34 @@ import Banner from "@/components/banner";
 import Sections from "@/components/sections";
 import Promotions from "@/components/promotions";
 import ServiceFeatures from "@/components/service-features";
+import useProductStore from "@/store/productStore";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { products, fetchProducts, shuffledProducts } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <main className="container mx-auto flex min-h-screen flex-col">
       <ContentHeadSection />
 
-      <Sections heading="sale" subheading="today"/>
+      <Sections heading="sale" subheading="today" products={products} />
       <Separator className="my-4" />
 
       <CategorySection />
       <Separator className="my-4" />
 
-      <Sections heading="best" subheading="month" />
+      <Sections heading="best" subheading="month" products={products} />
 
       <Banner />
 
-      <Sections heading="products" subheading="products" />
+      <Sections heading="products" subheading="products" products={products} />
 
-      <Promotions />
+      <Promotions shuffledProducts={shuffledProducts} />
 
       <ServiceFeatures />
     </main>
